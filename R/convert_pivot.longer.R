3f247a5997f90233f66a2cdff663841f632e69d9
Miyeon Yeon <sqr.yeon@gmail.com>
2023-08-10 13:04:56 -0500

    Convert a matrix to a data frame using pivot_longer

---
+++ R/convert_pivot.longer.R
@@ -0,0 +1,21 @@
+#' Convert a matrix to a data frame using pivot_longer
+#'
+#' @param mat a matrix with observations in rows, variables in columns, and values in cells
+#' @param rcenames row, column, and cell names of a matrix
+#' @references https://dcl-wrangle.stanford.edu/pivot-basic.html
+#' @import tidyverse
+#' @export
+
+convert_pivot.longer = function(mat, rcenames) {
+
+  row <- rownames(mat)
+  mat1 <- data.frame(row, mat)
+  mat2 <- pivot_longer(mat1,
+                        cols = !starts_with("row"),
+                        names_to = "col",
+                        values_to = "cell")
+  mat3 <- mat2[order(mat2$col, decreasing = FALSE), ]
+  colnames(mat3) <- rcenames
+
+  return(mat3)
+}
