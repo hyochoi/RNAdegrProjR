d4ac33ffa214b9df6916e4f0af08b951db8359be
Miyeon Yeon <sqr.yeon@gmail.com>
2023-08-27 19:33:32 -0400

    Update convert_pivot.longer.R

--- R/convert_pivot.longer.R
+++ R/convert_pivot.longer.R
@@ -3,17 +3,17 @@
 #' @param mat a matrix with observations in rows, variables in columns, and values in cells
 #' @param rcenames row, column, and cell names of a matrix
 #' @references https://dcl-wrangle.stanford.edu/pivot-basic.html
-#' @import tidyverse
+#' @import tidyr
 #' @export
 
 convert_pivot.longer = function(mat, rcenames) {
 
   row <- rownames(mat)
   mat1 <- data.frame(row, mat)
-  mat2 <- pivot_longer(mat1,
-                        cols = !starts_with("row"),
-                        names_to = "col",
-                        values_to = "cell")
+  mat2 <- tidyr::pivot_longer(mat1,
+                              cols = !starts_with("row"),
+                              names_to = "col",
+                              values_to = "cell")
   mat3 <- mat2[order(mat2$col, decreasing = FALSE), ]
   colnames(mat3) <- rcenames
 
